f94fa2534c877c01cc54bac8c771ed76a29ffcf4
Nouhaila Iguerd <125311341+nouhaila-17@users.noreply.github.com>
2023-04-27 08:20:29 +0000

    Add files via upload

---
+++ my_pictionary/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:my_pictionary/screens/draw_screen.dart';
+import 'package:my_pictionary/screens/guess_screen.dart';
+
+import 'package:my_pictionary/screens/main_choice.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'my_pictionary',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 83, 49, 157),
+        ),
+      ),
+      //route to other pages(widgets)
+      routes: {
+        MainChoice.routeName: (context) => MainChoice(context),
+        GuessUserScreen.routeName: (context) => GuessUserScreen(),
+        DrawUserScreen.routeName: (context) => DrawUserScreen(),
+      },
+
+      initialRoute: MainChoice.routeName,
+    );
+  }
+}
