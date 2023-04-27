89ea2f216488b77d3c16fc02322502c294d3ce69
Nouhaila <iguerdnouhaila@gmail.com>
2023-04-27 09:13:55 +0100

    connecting the firebase and setting up backend

--- my_pictionary/lib/main.dart
+++ my_pictionary/lib/main.dart
@@ -1,110 +1,37 @@
-// import 'package:flutter/material.dart';
-// import 'package:my_pictionary/screens/create_room_screen.dart';
-// import 'package:my_pictionary/screens/join_room_screen.dart';
-// import 'screens/main_room_choice.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({
-//     super.key,
-//   });
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'my_pictionary',
-//       theme: ThemeData(
-//         useMaterial3: true,
-//         colorScheme: ColorScheme.fromSeed(
-//           seedColor: const Color.fromARGB(255, 83, 49, 157),
-//         ),
-//       ),
-//       //route to other pages(widgets)
-//       routes: {
-//         MainRoomChoice.routeName: (context) => const MainRoomChoice(),
-//         JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
-//         CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
-//       },
-//       //initialRoute: MainRoomChoice.routeName,
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
-import 'package:painter/painter.dart';
+// import 'package:my_pictionary/screens/draw_screen.dart';
+// import 'package:my_pictionary/screens/guess_screen.dart';
 
-void main() => runApp(MyApp());
+// import 'package:my_pictionary/screens/main_choice.dart';
+
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+  });
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Painter Demo',
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final PainterController _controller = PainterController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          // Color Palette Rectangular
-          Container(
-            height: 100,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                Container(
-                  width: 100,
-                  color: Colors.red,
-                ),
-                Container(
-                  width: 100,
-                  color: Colors.blue,
-                ),
-                Container(
-                  width: 100,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 100,
-                  color: Colors.yellow,
-                ),
-              ],
-            ),
-          ),
-
-          // Drawing Rectangular
-          Expanded(
-            child: Painter(
-              _controller,
-            ),
-          ),
-
-          // Empty Rectangular
-          Container(
-            height: 50,
-          ),
-        ],
+    return MaterialApp(
+      title: 'my_pictionary',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 83, 49, 157),
+        ),
       ),
+      //route to other pages(widgets)
+      // routes: {
+      //   MainChoice.routeName: (context) => MainChoice(context),
+      //   GuessUserScreen.routeName: (context) => GuessUserScreen(),
+      //   DrawUserScreen.routeName: (context) => DrawUserScreen(),
+      // },
+
+      // initialRoute: MainChoice.routeName,
     );
   }
 }
